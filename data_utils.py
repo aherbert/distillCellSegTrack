179806b93d186e48b446eee4f1fd08fc4ce549a8
aherbert <a.herbert@sussex.ac.uk>
2023-09-28 11:24:34 +0100

    Remove image flat-field correction

--- data_utils.py
+++ data_utils.py
@@ -21,18 +21,13 @@ def make_tiles_and_reshape(img, bsize, augment, tile_overlap):
     ny, nx, nchan, ly, lx = tiles.shape
     return np.reshape(tiles, (ny*nx, nchan, ly, lx))
 
-def run_tiled(imgi, network, unnormed, augment=False, bsize=224, tile_overlap=0.1, ):
+def run_tiled(imgi, network, augment=False, bsize=224, tile_overlap=0.1, ):
     tiles = []
     channel_32_outputs = []
     final_outputs = []
 
     # make image into tiles
     IMG = make_tiles_and_reshape(imgi, bsize, augment, tile_overlap)
-
-    # make a flatfield correction from the original image into tiles
-    IMG_flatfield_corrected = flatfield_correction(unnormed)
-    IMG_flatfield_corrected = transforms.normalize_img(IMG_flatfield_corrected)
-    IMG_flatfield_corrected = make_tiles_and_reshape(IMG_flatfield_corrected, bsize, augment, tile_overlap)
 
     # process in batches
     batch_size = 8
@@ -41,33 +36,27 @@ def run_tiled(imgi, network, unnormed, augment=False, bsize=224, tile_overlap=0.
     for k in range(niter):
         irange = slice(batch_size * k, min(IMG.shape[0], batch_size * (k + 1)))
         input_img = torch.from_numpy(IMG[irange])
+        tiles.append(input_img)
 
-        # predict on the input image (uncorrected)
+        # predict on the input image
         channel_32_output, final_output = pred(input_img, network)
         channel_32_outputs.append(channel_32_output)
         final_outputs.append(final_output)
 
-        # create tiles of the flat-field corrected image
-        irange = slice(batch_size * k, min(IMG_flatfield_corrected.shape[0], batch_size * (k + 1)))
-        input_img = torch.from_numpy(IMG_flatfield_corrected[irange])
-        tiles.append(input_img)
-
     return tiles, channel_32_outputs, final_outputs
 
-def run_net(imgs, network, unnormed, augment=False, tile_overlap=0.1, bsize=224,
+def run_net(imgs, network, augment=False, tile_overlap=0.1, bsize=224,
                  # Unused
                  return_conv=False,return_training_data=False):
 
         # make image nchan x Ly x Lx for net
         imgs = np.transpose(imgs, (2,0,1))
-        unnormed = np.transpose(unnormed, (2,0,1))
 
         # pad image for net so Ly and Lx are divisible by 4
         imgs, _, _ = transforms.pad_image_ND(imgs)
-        unnormed, _, _ = transforms.pad_image_ND(unnormed)
 
         tiles, channel_32_outputs, final_outputs = run_tiled(imgs, network=network, augment=augment, bsize=bsize,
-                                    tile_overlap=tile_overlap,unnormed=unnormed)
+                                    tile_overlap=tile_overlap)
         return tiles, channel_32_outputs, final_outputs
 
 def run_cp(x, network, normalize=True, invert=False, augment=False): #, tile=True):
@@ -79,12 +68,11 @@ def run_cp(x, network, normalize=True, invert=False, augment=False): #, tile=Tru
     flows_and_cellprob_output = []
     for i in iterator:
         img = np.asarray(x[i])
-        img_unnormed = img.copy()
         if normalize or invert:
             img = transforms.normalize_img(img, invert=invert)
 
         tiles, channel_32_outputs, final_outputs = run_net(img, network=network, augment=augment, tile_overlap=0.1, bsize=224,
-                return_conv=False, return_training_data=True, unnormed=img_unnormed)
+                return_conv=False, return_training_data=True)
         tiled_images_input.append(tiles)
         intermdiate_outputs.append(channel_32_outputs)
         flows_and_cellprob_output.append(final_outputs)
@@ -146,38 +134,6 @@ def get_data_cp_clean(unet,combined_images):
 
     return tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final
 
-def gaussian_filter(pixel_data, sigma):
-    """Perform gaussian filter with the given radius"""
-    def fn(image):
-        return scipy.ndimage.gaussian_filter(
-            image, sigma, mode="constant", cval=0
-        )
-    mask = np.ones(pixel_data.shape)
-    bleed_over = fn(mask)
-    smoothed_image = fn(pixel_data)
-    output_image = smoothed_image / (bleed_over + np.finfo(float).eps)
-    return output_image
-
-def flatfield_correction(image):
-    if len(image.shape) == 2:
-        image = np.array([image])
-
-    sigma = 30
-
-    if image.shape[0] > 2:
-        corrected_per_channel = []
-        for channel in image:
-            blurred_img = gaussian_filter(channel, sigma)
-            blurred_img_meaned = blurred_img / blurred_img.mean()
-            result = channel / blurred_img_meaned
-            corrected_per_channel.append(result)
-        final_img = np.array(corrected_per_channel)
-    else:
-        blurred_img = gaussian_filter(image, sigma)
-        blurred_img_meaned = blurred_img / blurred_img.mean()
-        final_img = image / blurred_img_meaned
-    return final_img
-
 def adjust_brightness(image, brightness_factor):
     # Ensure the image has values in the range [0, 1]
     #image = torch.clamp(image, 0.0, 1.0)
@@ -195,7 +151,6 @@ def apply_rotation(original_image):
     # Rotate by 270 degrees
     rotated_270 = torch.rot90(original_image, k=3, dims=(1, 2))
     return rotated_90, rotated_180, rotated_270
-
 
 def brightness_augmentation(tiled_images_final, intermediate_outputs_final, flows_and_cellprob_output_final):
     brightness_min = 0.1
