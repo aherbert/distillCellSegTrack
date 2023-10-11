fe44c3d9b0b4d71632c9709083b6c43ca3430f67
aherbert <a.herbert@sussex.ac.uk>
2023-10-11 13:33:28 +0100

    Add Cellpose extensions to allow saving input/output tiles

--- cellpose_ext.py
+++ cellpose_ext.py
@@ -1,10 +1,39 @@
-#
-# Extend the CellPose network to provide access to the penultimate
-# layer in the network
-#
+# ------------------------------------------------------------------------------
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+# OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+# ------------------------------------------------------------------------------
+
+# CellPose extensions
+
+import numpy as np
+import os
+import torch
+import numba as nb
 
 from cellpose.resnet_torch import CPnet
+from cellpose.models import CellposeModel
+from torch.utils import mkldnn as mkldnn_utils
 
+# short-circuiting replacement for np.any()
+# see: https://stackoverflow.com/a/53474543
+@nb.jit(nopython=True)
+def _sc_any(array):
+    for x in array.flat:
+        if x:
+            return True
+    return False
+
+# Extend the CellPose network to provide access to the penultimate
+# layer in the network
+# Adapted from cellpose/resnet_torch.py
 class CPnetX(CPnet):
     def __init__(self, nbase, nout, sz,
                 residual_on=True, style_on=True,
@@ -34,6 +63,141 @@ class CPnetX(CPnet):
 
         if self.mkldnn:
             T0 = T0.to_dense()
-            #T1 = T1.to_dense()
+            T0_32 = T0_32.to_dense()
         # Return in the same order as cellpose and append the 32-channel layer
         return T0, style0, T0_32
+
+# Extend the CellposeModel to use the custom CellPose network.
+# Adapted from cellpose/models.py
+class CellposeModelX(CellposeModel):
+    """
+
+    Parameters
+    -------------------
+
+    gpu: bool (optional, default False)
+        whether or not to save model to GPU, will check if GPU available
+        
+    pretrained_model: str or list of strings (optional, default False)
+        full path to pretrained cellpose model(s), if None or False, no model loaded
+        
+    model_type: str (optional, default None)
+        any model that is available in the GUI, use name in GUI e.g. 'livecell' 
+        (can be user-trained or model zoo)
+        
+    net_avg: bool (optional, default False)
+        loads the 4 built-in networks and averages them if True, loads one network if False
+        
+    diam_mean: float (optional, default 30.)
+        mean 'diameter', 30. is built in value for 'cyto' model; 17. is built in value for 'nuclei' model; 
+        if saved in custom model file (cellpose>=2.0) then it will be loaded automatically and overwrite this value
+        
+    device: torch device (optional, default None)
+        device used for model running / training 
+        (torch.device('cuda') or torch.device('cpu')), overrides gpu input,
+        recommended if you want to use a specific GPU (e.g. torch.device('cuda:1'))
+
+    residual_on: bool (optional, default True)
+        use 4 conv blocks with skip connections per layer instead of 2 conv blocks
+        like conventional u-nets
+
+    style_on: bool (optional, default True)
+        use skip connections from style vector to all upsampling layers
+
+    concatenation: bool (optional, default False)
+        if True, concatentate downsampling block outputs with upsampling block inputs; 
+        default is to add 
+    
+    nchan: int (optional, default 2)
+        number of channels to use as input to network, default is 2 
+        (cyto + nuclei) or (nuclei + zeros)
+    
+    save_directory: str (optional, default None)
+        directory to save input to and output from the network (images
+        will be saved with a counter suffix)
+    """
+    
+    def __init__(self, gpu=False, pretrained_model=False, 
+                    model_type=None, net_avg=False,
+                    diam_mean=30., device=None,
+                    residual_on=True, style_on=True, concatenation=False,
+                    nchan=2,
+                    save_directory=None):
+        super(CellposeModelX, self).__init__(
+            gpu=gpu, pretrained_model=pretrained_model, 
+            model_type=model_type, net_avg=net_avg,
+            diam_mean=diam_mean, device=device,
+            residual_on=residual_on, style_on=style_on, concatenation=concatenation,
+            nchan=nchan)
+        
+        # Validate save directory
+        if save_directory:
+            if not os.path.isdir(save_directory):
+                raise NotADirectoryError(save_directory)
+            self._save_directory = save_directory
+        self._count = 0
+
+        # The network is created in cellpose.core.UnetModel.
+        # Here we replace the network with our custom version.
+        # Code adapted from cellpose/core.py:UnetModel.__init__
+        self.net = CPnetX(self.nbase, 
+                          self.nclasses, 
+                          sz=3,
+                          residual_on=residual_on, 
+                          style_on=style_on,
+                          concatenation=concatenation,
+                          mkldnn=self.mkldnn,
+                          diam_mean=diam_mean).to(self.device)
+
+        # Reinitialise network        
+        # Code adapted from cellpose/models.py:CellposeModel.__init__
+        # This duplication of loading a model roughly doubles construction time
+        # from ~1.2s to 2.4s
+        if self.pretrained_model:
+            self.net.load_model(self.pretrained_model[0], device=self.device)
+            self.diam_mean = self.net.diam_mean.data.cpu().numpy()[0]
+            self.diam_labels = self.net.diam_labels.data.cpu().numpy()[0]
+            # Removed logging of model details
+
+    def network(self, x, return_conv=False):
+        """ convert imgs to torch and run network model and return numpy """
+        # This method is copied from the UnetModel super-class with
+        # modifications to save the network input and output
+
+        X = self._to_device(x)
+        self.net.eval()
+        if self.mkldnn:
+            self.net = mkldnn_utils.to_mkldnn(self.net)
+        with torch.no_grad():
+            y, style, y32 = self.net(X)
+        del X
+        y = self._from_device(y)
+        style = self._from_device(style)
+       
+        # Commented out. This appears to be legacy code that is not used
+        # since conv is not defined.
+        #if return_conv:
+        #    conv = self._from_device(conv)
+        #    y = np.concatenate((y, conv), axis=1)
+
+        # Save input/output
+        if self._save_directory:
+            y32 = self._from_device(y32)
+            
+            # save individual tiles
+            # Tiles dimensions: ntiles x nchan x bsize x bsize
+            # (bsize = size of tiles)
+            n = len(x)
+            for i in range(n):
+                a = x[i]
+                # greyscale images are padded with a zero channel
+                # which can be removed to save space
+                if not _sc_any(a[1]):
+                    a = a[0].squeeze()
+                np.save(os.path.join(self._save_directory, f'input_{self._count+i}.npy'), a)
+                np.save(os.path.join(self._save_directory, f'output_{self._count+i}.npy'), y[i])
+                np.save(os.path.join(self._save_directory, f'style_{self._count+i}.npy'), style[i])
+                np.save(os.path.join(self._save_directory, f'output32_{self._count+i}.npy'), y32[i])
+            self._count += n
+       
+        return y, style
---
+++ create_dataset.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+# ------------------------------------------------------------------------------
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+# OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+# ------------------------------------------------------------------------------
+
+import argparse
+import os
+
+def file_or_dir_path(string):
+    if os.path.isfile(string) or os.path.isdir(string):
+        return string
+    else:
+        raise FileNotFoundError(string)
+
+def file_path(string):
+    if os.path.isfile(string):
+        return string
+    else:
+        raise FileNotFoundError(string)
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+def run(args):
+    import time
+    import logging
+    import numpy as np
+    import psutil
+    import glob
+    import torch
+    import shutil
+    from cellpose_ext import CellposeModelX
+
+    # Debug memory usage
+    if args.memory:
+        logging.basicConfig(
+            format='[%(asctime)s] %(levelname)s - %(mem)s - %(message)s',
+            level=logging.INFO)
+
+        old_factory = logging.getLogRecordFactory()
+
+        def record_factory(*args, **kwargs):
+            record = old_factory(*args, **kwargs)
+            process = psutil.Process()
+            i = process.memory_info()
+            record.mem = f'rss={i.rss/1024**3:8.4f}, vms={i.vms/1024**3:8.4f}'
+            return record
+
+        logging.setLogRecordFactory(record_factory)
+    else:
+        logging.basicConfig(
+            format='[%(asctime)s] %(levelname)s - %(message)s',
+            level=logging.INFO)
+
+    start_time = time.time()
+    
+    save_directory=f'{args.save_dir}_{os.path.basename(args.model)}'
+    if os.path.isdir(save_directory):
+        if not args.delete:
+            raise Exception(f'Save directory exists: {save_directory}')
+        # Clean output directory
+        for root, dirs, files in os.walk(save_directory):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
+    else:
+        os.makedirs(save_directory)
+
+    # Find input images
+    combined_images = []
+    images = args.image
+    if np.isscalar(images):
+        images = [images]
+    for f in images:
+        if os.path.isdir(f):
+            for filename in glob.glob(os.path.join(f, '*.npy')):
+                combined_images.append(filename)
+        elif os.path.isfile(f):
+            combined_images.append(f)
+        else:
+            raise FileNotFoundError(f)
+
+    # Configure Cellpose arguments
+    # The diameters are currently coded using defaults from Omero Screen.
+    if args.cyto:
+        # [[channel to segment, optional nuclear channel]]
+        channels = [2, 1]
+        diameter = 40
+    else:
+        # to segment grayscale images, input [0,0]
+        channels = [0, 0]
+        diameter = 10
+    device = torch.device(args.device)
+
+    # Create Cellpose with option to save input/output
+    segmentation_model = CellposeModelX(
+        model_type=os.path.abspath(args.model),
+        device=device,
+        save_directory=save_directory
+    )
+
+    # Run Cellpose
+    for i, image in enumerate(combined_images):
+        logging.info(f'Processing image {i}: {image}')
+        img = np.load(image)
+
+        # Image should be 2D/3D (with channels)
+        # Cellpose requires XYC
+        # (Note: Although the CellposeModel.exec method is documented as
+        # Z x nchan x Y x X the internal code reshapes the data to channels last.)
+        if args.cyto:
+            # Require 2 channels
+            if img.ndim != 3:
+                raise Exception('{image} requires 2 channels')
+            img = np.dstack([img[args.nuclei_channel-1], img[args.cyto_channel-1]])
+        else:
+            if img.ndim == 3:
+                img = img[args.nuclei_channel-1]
+            elif img.ndim != 2:
+                raise Exception('{image} requires 1 channel')
+
+        # TODO: Should this be saved?
+        start = segmentation_model._count
+        c_masks_array, c_flows, c_styles = segmentation_model.eval(
+            img, channels=channels,
+            diameter=diameter, normalize=False
+        )
+        logging.info(f'Saved {save_directory}: {start}-{segmentation_model._count - 1}')
+
+    t = time.time() - start_time
+    logging.info(f'Done (in {t} seconds)')
+
+if __name__ == '__main__':
+    base = os.path.dirname(os.path.abspath(__file__));
+    cellpose_model = os.path.join(base, "cellpose_models", "Nuclei_Hoechst")
+
+    parser = argparse.ArgumentParser(
+      description='Program to convert input images ([C] x X x Y) to a dataset.' + 
+          ' Images are assumed to be normalised to [0, 1].')
+
+    parser.add_argument('image', nargs='+', metavar='IMAGE',
+        type=file_or_dir_path,
+        help='image or image directory')
+    parser.add_argument('-m', '--model', dest='model', type=file_path,
+        default=cellpose_model,
+        help='CellPose model (default: %(default)s)')
+    parser.add_argument('--cyto-channel', dest='cyto_channel', type=int,
+        default=2,
+        help='Cytoplasm channel (1-based index) (default: %(default)s)')
+    parser.add_argument('--nuclei-channel', dest='nuclei_channel', type=int,
+        default=1,
+        help='Nuclei channel (1-based index) (default: %(default)s)')
+    parser.add_argument('--cyto', dest='cyto', action='store_true',
+        help='Cytoplasm model (run with 2 channels; defaults to nuclei channel only)')
+    parser.add_argument('-d', '--device', dest='device',
+        default='cuda',
+        help='Device (default: %(default)s)')
+    parser.add_argument('--memory', dest='memory', action='store_true',
+        help='Debug memory usage')
+    parser.add_argument('-s', '--save', dest='save_dir', type=str,
+        default='test_data',
+        help='Save directory prefix (default: %(default)s)')
+    parser.add_argument('--delete', dest='delete', action='store_true',
+        help='Delete existing data (default is to error)')
+
+    args = parser.parse_args()
+    run(args)
