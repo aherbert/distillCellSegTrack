3dc10ef40b4b47bec7737ee1103b034033d23894
aherbert <a.herbert@sussex.ac.uk>
2023-11-07 11:30:53 +0000

    Remove schebang

--- cp_distill/image_utils.py
+++ cp_distill/image_utils.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # ------------------------------------------------------------------------------
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted.
