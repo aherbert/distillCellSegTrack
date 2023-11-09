Fix testing IoU computation

bin/create_dataset.py
@@ -128,13 +128,15 @@ def run(args):
             elif img.ndim != 2:
                 raise Exception('{image} requires 1 channel')
 
-        # Do rotations
+        # Do rotations. Save the first rotation for the testing data.
+        imgs = None
         for k in args.rotations:
             logging.info(f'Processing rotation {k} on {i+1}: {image}')
 
             start = segmentation_model._count
+            rotated = np.rot90(img, k=k, axes=(0, 1))
             masks_array, flows, styles = segmentation_model.eval(
-                np.rot90(img, k=k, axes=(0, 1)),
+                rotated,
                 channels=channels,
                 batch_size=args.batch_size,
                 diameter=diameter, normalize=False
@@ -142,14 +144,16 @@ def run(args):
             logging.info(f'Saved {save_directory}: {start}-{segmentation_model._count - 1}')
 
             # Create validation data
-            if k == 0:
+            if imgs == None:
                 orig_dim = img.shape[:2] if args.cyto else img.shape
-                imgs = resize_image(img, rsz=segmentation_model.last_rescale,
+                rescale = segmentation_model.last_rescale
+                imgs = resize_image(rotated, rsz=rescale,
                     no_channels=not args.cyto)
                 logging.log(15, 'Resized %s to %s', img.shape, imgs.shape)
                 # From cellpose.core.UnetModel._run_net
-                # Make image nchan x Ly x Lx for net
-                imgs = np.transpose(imgs, (2,0,1)) if args.cyto else imgs[np.newaxis, ...]
+                # Make image nchan x Ly x Lx for net.
+                # Put the channel to segment first (assume cyto channels = [2, 1])
+                imgs = np.transpose(imgs[..., [1, 0]], (2,0,1)) if args.cyto else imgs[np.newaxis, ...]
                 # pad image for net so Ly and Lx are divisible by 16
                 imgs, ysub, xsub = pad_image_ND(imgs)
                 # slices from padding
@@ -171,7 +175,7 @@ def run(args):
                 np.save(os.path.join(save_directory, f'tiles_{i+1}.npy'), tiles)
                 np.save(os.path.join(save_directory, f'mask_{i+1}.npy'), m1)
                 with open(os.path.join(save_directory, f'tiles_{i+1}.pkl'), 'wb') as f:
-                    pickle.dump((orig_dim, slc, tile_dim, ysub, xsub, Ly, Lx), f)
+                    pickle.dump((orig_dim, rescale, slc, tile_dim, ysub, xsub, Ly, Lx), f)
                 logging.info(f'Saved validation tiles {save_directory}: {i+1} {tiles.shape}')
 
             if not args.compute_flows:

cp_distill/testing.py
@@ -20,7 +20,6 @@ from cp_distill.image_utils import filter_segmentation
 import warnings
 
 def test_network(net, test_data, device, batch_size,
-                 niter=200, 
                  cellprob_threshold=0.0,
                  flow_threshold=0.4, interp=True,
                  min_size=15):
@@ -37,6 +36,15 @@ def test_network(net, test_data, device, batch_size,
         Torch device. The network model must be loaded to the same device.
     batch_size : int
         Batch size.
+    cellprob_threshold: float (optional, default 0.0) 
+        all pixels with value above threshold kept for masks, decrease to find more and larger masks
+    flow_threshold: float (optional, default 0.4)
+        flow error threshold (all cells with errors below threshold are kept)
+    interp: bool (optional, default True)
+        interpolate during 2D dynamics
+        (in previous versions it was False)
+    min_size: int (optional, default 15)
+        minimum number of pixels per mask, can turn off with -1
 
     Returns
     -------
@@ -54,7 +62,7 @@ def test_network(net, test_data, device, batch_size,
         # cellpose.core.UnetModel._run_net: to pad the image
         # cellpose.core.UnetModel._run_tiled: to make tiles
         
-        orig_dim, slc, tile_dim, ysub, xsub, Ly, Lx = data
+        orig_dim, rescale, slc, tile_dim, ysub, xsub, Ly, Lx = data
         # From cellpose.core.UnetModel._run_tiled
         n, nchan, ly, lx = IMG.shape
         niter = int(np.ceil(IMG.shape[0] / batch_size))
@@ -70,9 +78,9 @@ def test_network(net, test_data, device, batch_size,
             # cellpose.core.UnetModel._from_device
             y0 = y0.detach().cpu().numpy()
             y[irange] = y0.reshape(irange.stop-irange.start, y0.shape[-3], y0.shape[-2], y0.shape[-1])
-        
+
         y = average_tiles(y, ysub, xsub, Ly, Lx)
-        # Here we reconstrut the size of the image before calling make_tiles
+        # Here we reconstruct the size of the image before calling make_tiles
         y = y[:,:tile_dim[0],:tile_dim[1]]
         
         # From cellpose.core.UnetModel._run_net
@@ -80,11 +88,12 @@ def test_network(net, test_data, device, batch_size,
         y = y[slc]
         # transpose so channels axis is last again
         y = np.transpose(y, (1,2,0))
-        
+
         # From cellpose.models.CellposeModel._run_cp
         y = resize_image(y, orig_dim[0], orig_dim[1])
         cellprob = y[:,:,2]
         dP = y[:,:,:2].transpose((2,0,1))
+        niter = (1 / rescale * 200)
         masks_array, _ = compute_masks(dP, cellprob, niter=niter,
             cellprob_threshold=cellprob_threshold,
             flow_threshold=flow_threshold, interp=interp, 